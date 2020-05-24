cec2127f73ad00325160ef95229fc4f350de91e0
anastasiya-h79 <anastasiya-h79@yandex.ru>
2020-05-24 12:52:36 +0300

    сделала страничку checkout.html и thanks.html. Исправила ошибку с подсчетом общей цены заказа в админке, теперь все работает

--- vrhelmet/boxapp/views.py
+++ vrhelmet/boxapp/views.py
@@ -6,20 +6,24 @@ from django.views.generic.base import ContextMixin
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.shortcuts import get_list_or_404
 
-from .models import ProductInOrder, UserInfo, Delivery, MethodPay
+from .models import Order, ProductInOrder, UserInfo, Delivery, MethodPay
 from prodapp.views import DateContextMixin
 
 from django.http import JsonResponse
 from .models import ProductInBasket
+from .forms import CheckoutContactForm
+from usersapp.models import SiteUser
+
+from django.contrib.auth.models import User
 
 # Create your views here.
 # вьюшка для корзины заказа с правами для залогиненного текущего пользователя
-class OrderListView(LoginRequiredMixin, ListView, DateContextMixin):
-    model = ProductInOrder
-    template_name = 'boxapp/order.html'
-
-    def test_func(self):
-        return self.request.user
+# class OrderListView(LoginRequiredMixin, ListView, DateContextMixin):
+#     model = ProductInOrder
+#     template_name = 'boxapp/checkout.html'
+#
+#     def test_func(self):
+#         return self.request.user
 
 
 def basket_adding(request):
@@ -57,3 +61,46 @@ def basket_adding(request):
         return_dict["products"].append(product_dict)
 
     return JsonResponse(return_dict)
+
+def checkout(request):
+    session_key = request.session.session_key
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True, order__isnull=True)
+    print(products_in_basket)
+    for item in products_in_basket:
+        print(item.order)
+
+
+    form = CheckoutContactForm(request.POST or None)
+    if request.POST:
+        print(request.POST)
+        if form.is_valid():
+            print("yes")
+            data = request.POST
+            name = data.get("name", "3423453")
+            phone = data["phone"]
+            #email = data.get("email", "test@test.com")
+            user, created = SiteUser.objects.get_or_create(username=phone, defaults={"first_name": name})
+
+
+            order = Order.objects.create(user=user, customer_name=name, customer_phone=phone, status_id=1)
+
+            for name, value in data.items():
+                if name.startswith("product_in_basket_"):
+                    product_in_basket_id = name.split("product_in_basket_")[1]
+                    product_in_basket = ProductInBasket.objects.get(id=product_in_basket_id)
+                    print(type(value))
+
+                    product_in_basket.nmb = int(value)
+                    product_in_basket.order = order
+                    product_in_basket.save(force_update=True)
+
+                    ProductInOrder.objects.create(product=product_in_basket.product, nmb=product_in_basket.nmb,
+                                                  price_per_item=product_in_basket.price_per_item,
+                                                  total_price=product_in_basket.total_price,
+                                                  order=order)
+
+            #return HttpResponseRedirect(request.META['HTTP_REFERER'])
+            return render(request, 'boxapp/thanks.html')
+        else:
+            print("no")
+    return render(request, 'boxapp/checkout.html', locals())
